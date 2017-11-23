664431687a50597ecec2c79031b8d80404970e37
ann <ann.hvarregaard@gmail.com>
2017-11-23 10:46:19 +0100

    Lag, nye mapper mm.

--- Web/Controllers/BookingController.cs
+++ Web/Controllers/BookingController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
@@ -16,8 +17,11 @@ namespace Web.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            BookingVM hest = new BookingVM();
+            hest.Name = "book tid";
 
-            return View();
+
+            return View(hest);
         }
 
         public ActionResult Contact()
---
+++ Web/Models/BookingVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class BookingVM
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
