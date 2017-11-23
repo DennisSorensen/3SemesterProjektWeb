cdf01c8ced4642058c8474743338d3e757a65877
Bodil <bodil.noergaard86@gmail.com>
2017-11-23 12:27:40 +0100

    service ref opstart

--- Web/BLL/BookingController.cs
+++ Web/BLL/BookingController.cs
@@ -7,5 +7,30 @@ namespace Web.BLL
 {
     public class BookingController
     {
+    
+        public BookingController()
+        {
+
+        }
+
+        public void Create (Booking booking)
+        {
+
+        }
+
+        public void Delete(int Id)
+        {
+
+        }
+
+        public Booking Get(int Id)
+        {
+
+        }
+
+        public void Edit(Booking booking)
+        {
+
+        }
     }
 }
