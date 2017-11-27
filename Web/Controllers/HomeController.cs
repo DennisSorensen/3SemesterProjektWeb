b1d35e4fb4b6afde09afcb251b99602191cf6708
Bodil <bodil.noergaard86@gmail.com>
2017-11-27 11:06:29 +0100

    home og kontakt færdig

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -13,14 +13,14 @@ namespace Web.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Booking()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Kontakt()
         {
             ViewBag.Message = "Your contact page.";
 
