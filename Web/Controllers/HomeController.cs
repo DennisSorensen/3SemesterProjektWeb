ac91693bf00e309f8b2cec1112bb9e9d46da9ae6
Bodil <bodil.noergaard86@gmail.com>
2017-11-27 13:06:50 +0100

    booking site

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
@@ -15,14 +16,15 @@ namespace Web.Controllers
 
         public ActionResult Booking()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            BookingVM model = new BookingVM();
+            model.Departments = new List<DepartmentVM>();
+            
+            return View(model);
         }
 
         public ActionResult Kontakt()
         {
-            ViewBag.Message = "Your contact page.";
+           
 
             return View();
         }
---
+++ Web/Models/BookingVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class BookingVM
+    {
+        public List<DepartmentVM> Departments { get; set; }
+    }
+
+}
